9be817041571c3e656627d4b636cb7b4d268e0e4
Andy Evans <andy.evans@myknowledgemap.co.uk>
2021-08-15 17:56:55 +0100

    initial commit of solution and console project

---
+++ Minefield/Minefield/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minefield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
